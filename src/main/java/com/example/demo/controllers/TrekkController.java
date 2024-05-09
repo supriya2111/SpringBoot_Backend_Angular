b05d582dedec938dc8839a6cd8b572221a673c31
Supriya Sutar <supriyasutar543@gmail.com>
2024-05-09 23:48:13 +0530

    method added for update

--- src/main/java/com/example/demo/controllers/TrekkController.java
+++ src/main/java/com/example/demo/controllers/TrekkController.java
@@ -3,18 +3,17 @@ package com.example.demo.controllers;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.demo.entities.Trekk;
 import com.example.demo.services.TrekkService;
-
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestParam;
 
 
 
@@ -44,6 +43,11 @@ public class TrekkController {
 
     @GetMapping("/trekks/{id}")
     public Trekk getTrekk(@PathVariable int id) {
+       return tservice.getTrekk(id);
+    }
+    
+    @GetMapping("/trekks/update/{id}")
+    public Trekk getTrekkforUpdate(@PathVariable int id) {
        return tservice.getTrekk(id);
     }
     
